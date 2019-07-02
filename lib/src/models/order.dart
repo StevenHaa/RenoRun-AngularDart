17ed15c2b25b160ddc540d43a49cdde8f8e56db1
Steven Ha <steven_ha666@hotmail.com>
2019-07-02 16:21:28 -0400

    create empty constructor and toString method

--- lib/src/models/order.dart
+++ lib/src/models/order.dart
@@ -2,10 +2,20 @@ class Order {
   String email;
   String colour;
   int productId;
+  
+  Order.empty() {
+    this.email = "";
+    this.colour = "";
+    this.productId = 0;
+  }
 
   Order(String email, String colour, int productId) { 
     this.email = email;
     this.colour = colour;
     this.productId = productId;
+  }
+
+  String toString() {
+    return '$email, $colour $productId';
   }
 }
