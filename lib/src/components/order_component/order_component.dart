be72f731084ab07d556f1f810c01070f1e447940
Steven Ha <steven_ha666@hotmail.com>
2019-07-02 22:27:41 -0400

    add email validation

--- lib/src/components/order_component/order_component.dart
+++ lib/src/components/order_component/order_component.dart
@@ -1,6 +1,7 @@
 import 'package:RenoRun_AngularDart/src/models/order.dart';
 import 'package:angular/angular.dart';
 import 'package:angular_forms/angular_forms.dart';
+import 'dart:core';
 
 @Component(
 	selector: 'order',
@@ -16,11 +17,26 @@ class OrderComponent implements OnInit{
   
   Order model = new Order.empty();
 
+  bool validEmail;
+
   // List<String> colors = ["gold", "blue", "red"];
   String chosenColor;
 
   @override
   void ngOnInit() {
+  }
+
+  // function to see if the email field is empty or not
+  void validateEmail(event) {
+    String email = event.target.value;
+    RegExp regex = new RegExp("^[\\w-_\\.+]*[\\w-_\\.]\\@([\\w]+\\.)+[\\w]+[\\w]\$");
+
+    if(regex.hasMatch(email)){
+      validEmail = true;
+    }
+    else {
+      validEmail = false;
+    }
   }
 
   // function to change the colour chosen
@@ -42,5 +58,6 @@ class OrderComponent implements OnInit{
     model.email = "";
     model.colour = "";
     chosenColor = null;
+    validEmail = false;
   }
 }
