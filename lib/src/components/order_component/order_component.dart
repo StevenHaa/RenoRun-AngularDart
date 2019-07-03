a7816fa4f13f59450059ef0bebe0cbebac0172fd
Steven Ha <steven_ha666@hotmail.com>
2019-07-02 21:18:16 -0400

    add custom css for checkmark and submit form funct

--- lib/src/components/order_component/order_component.dart
+++ lib/src/components/order_component/order_component.dart
@@ -1,6 +1,6 @@
+import 'package:RenoRun_AngularDart/src/models/order.dart';
 import 'package:angular/angular.dart';
 import 'package:angular_forms/angular_forms.dart';
-import 'package:RenoRun_AngularDart/src/services/order_service.dart';
 
 @Component(
 	selector: 'order',
@@ -10,6 +10,31 @@ import 'package:RenoRun_AngularDart/src/services/order_service.dart';
 	directives: [coreDirectives, formDirectives],
 )
 
-class OrderComponent {
-  // OrderService orderService;
+class OrderComponent implements OnInit{
+  // productId value is set when the parents calls the selector with this [attrib]
+  @Input() var productId;
+  
+  Order model = new Order.empty();
+
+  // List<String> colors = ["gold", "blue", "red"];
+  String chosenColor;
+
+  @override
+  void ngOnInit() {
+  }
+
+  // function to change the colour chosen
+  void updateColour(String colour) {
+    print('Picked : $colour');
+    model.colour = colour;
+    chosenColor = colour;
+  }
+
+  // function when submitting the form
+  void onSubmit(int productId) {
+    model.productId = productId;
+    print('onSubmit : $model');
+  }
+
+  
 }
